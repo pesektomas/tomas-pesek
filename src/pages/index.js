e232c5902ef91e417a33367fe909a44a61dae4a1
Tomas Pesek <pesek@economia.cz>
2019-01-15 23:09:43 +0100

    Add react ga and change date format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import ReactGA from 'react-ga';
 
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
+
+    ReactGA.initialize('UA-81225037-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
 
     return (
       <section className="section">
@@ -67,7 +71,7 @@ export const pageQuery = graphql`
           frontmatter {
             title
             templateKey
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "DD. MM YYYY")
           }
         }
       }
